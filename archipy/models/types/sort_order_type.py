ffe24d2819bf0fb2c895c2589de243859f9f342f
h.nejati <hosseinnejati14@gmail.com>
2025-02-22 18:43:28 +0330

    Change Ruff Config

--- archipy/models/types/sort_order_type.py
+++ archipy/models/types/sort_order_type.py
@@ -2,5 +2,15 @@ from enum import Enum
 
 
 class SortOrderType(Enum):
+    """Enumeration of sorting order types.
+
+    This enum defines the types of sorting orders that can be applied to data,
+    such as ascending or descending.
+
+    Attributes:
+        ASCENDING (str): Represents sorting in ascending order.
+        DESCENDING (str): Represents sorting in descending order.
+    """
+
     ASCENDING = "ascending"
     DESCENDING = "descending"
